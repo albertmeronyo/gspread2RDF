8123619e56e1ec5cd4653b8394dd8466d84eda61
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2014-04-02 17:43:35 +0200

    FTW

---
+++ gspread2RDF.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from rdflib import URIRef, Literal, Graph
+from rdflib.namespace import SKOS
+import gspread
+
+# Graph
+g = Graph()
+
+# Google spreadsheets login
+gc = gspread.login('albert.meronyo@gmail.com', 
+                   'Civ33a4kXuc22l3j')
+
+# Open worksheet
+wb = gc.open("Links HISCO and ICONCLASS")
+wks = wb.get_worksheet(2)
+
+# Fetch the data
+icURIs = wks.col_values(4)
+icDescs = wks.col_values(3)
+hiscoURIs = wks.col_values(7)
+hiscoDescs = wks.col_values(6)
+
+for i in range(2,len(icURIs)-1):
+    try:
+        icURI = URIRef(icURIs[i])
+    except:
+        next
+    icDesc = Literal(icDescs[i])
+    try:
+        hiscoURI = URIRef("http://cedar.example.org/ns#hisco-" + hiscoURIs[i])
+    except:
+        next
+    hiscoDesc = Literal(hiscoDescs[i])
+
+    print icURI, icDesc, hiscoURI, hiscoDesc
+    g.add( (icURI, SKOS.prefLabel, icDesc) )
+    g.add( (icURI, SKOS.exactMatch, hiscoURI) )
+    g.add( (hiscoURI, SKOS.prefLabel, hiscoDesc) )
+
+g.serialize('gspread-ic-hisco.ttl', format="turtle")
+
+
+
