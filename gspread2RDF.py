d32b14f4c3d36ab0b0222685ed1ffaeec2ba6356
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2014-04-14 15:41:41 +0200

    Not next, continue plz kthxbye

--- gspread2RDF.py
+++ gspread2RDF.py
@@ -21,24 +21,26 @@ icDescs = wks.col_values(3)
 hiscoURIs = wks.col_values(7)
 hiscoDescs = wks.col_values(6)
 
+linkCount = 0
 for i in range(2,len(icURIs)-1):
     try:
         icURI = URIRef(icURIs[i])
     except:
-        next
+        continue
     icDesc = Literal(icDescs[i])
     try:
         hiscoURI = URIRef("http://cedar.example.org/ns#hisco-" + hiscoURIs[i])
     except:
-        next
+        continue
     hiscoDesc = Literal(hiscoDescs[i])
 
     print icURI, icDesc, hiscoURI, hiscoDesc
     g.add( (icURI, SKOS.prefLabel, icDesc) )
     g.add( (icURI, SKOS.exactMatch, hiscoURI) )
     g.add( (hiscoURI, SKOS.prefLabel, hiscoDesc) )
+    linkCount += 1
 
 g.serialize('gspread-ic-hisco.ttl', format="turtle")
-
+print "Serialized %s links" % str(linkCount)
 
 
